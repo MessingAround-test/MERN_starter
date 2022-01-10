1ffe66b91052cb398839add9d548e5b0726feaee
Bryn <lombryn@gmail.com>
2022-01-11 00:12:25 +1100

    added mongo connection to API

--- server/src/index.js
+++ server/src/index.js
@@ -6,10 +6,32 @@ const logger = require("morgan")
 const mongoose = require('mongoose')
 app.use(logger('dev'));
 
+// connects to the mongo DB 
+connect()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async(req, res) => {
+  // await connect()
+  var item = await find()
+  // console.log(item)
+  res.send(item)
 })
+
+// Creates a schema in our DB with this look, if it already exists then good :) we can query it
+const testModel = mongoose.model('testCollection', new mongoose.Schema({ name: String, age: Number }), 'testCollection');
+
+async function connect() {
+  await mongoose.connect('mongodb://admin:admin@db:27017/');
+  
+}
+
+// Search the schema we created above to see if any data is in there.
+async function find() {
+  const doc = await testModel.find({});
+  return doc
+}
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
